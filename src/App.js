2fe6543af19f4a4d148c33b722f36865b117fb5a
Rokas <rookas.rudzenskas@gmail.com>
2021-04-25 11:47:53 +0300

    ROkas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+    const [input, setInput] = useState('')
+
+
   return (
     <div className="App">
      <h1>Hello World 🚀</h1>
+
+
+        <input type="text"/>
+        <button>Send Message</button>
+    {/*    input    */}
+
+
+    {/*    button    */}
+    {/*    messages themselves    */}
     </div>
   );
 }
