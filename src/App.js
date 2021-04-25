c96c68eaa5d758ea1b063c2052c9d433d1a4047c
Rokas <rookas.rudzenskas@gmail.com>
2021-04-25 12:19:56 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState('');
+    const [messages, setMessages] = useState(['hello', 'what is up']);
+
+    console.log('🔫' + input);
+    console.log(messages);
+
+    const sendMessage = (event) => {
+        // all the logic to send message goes here
+        setMessages([...messages, input]);
+        setInput('');
+
+    }
 
 
   return (
@@ -10,8 +21,8 @@ function App() {
      <h1>Hello World 🚀</h1>
 
 
-        <input type="text"/>
-        <button>Send Message</button>
+        <input type="text" value={input} onChange={event => setInput(event.target.value)}/>
+        <button onClick={sendMessage}>Send Message</button>
     {/*    input    */}
 
 
